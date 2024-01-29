a35df8b8cb0f0f2eb27a3a1d2b80c440f63f376e
Mahid Ahmad <mahid@workstory.io>
2024-01-29 06:09:44 -0500

    First Commit

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode, useState } from 'react'
+
+interface Props {
+    children: string;
+    maxChars?: number;
+    onClick?: () => void;
+}
+
+const ExpandableText = ({children, maxChars = 100}: Props ) => {
+    const [isExpanded, setExpansion] = useState(false);
+
+    if (children.length <= maxChars) return <p>{children}</p>;
+
+    const text = isExpanded ? children : children.substring(0, maxChars);
+
+    return (
+        <p>
+            {text} {' '}
+            <button
+                style={{borderRadius : '5px', borderColor : 'transparent', backgroundColor : 'wheat', color : 'black'}}
+                onClick={() => setExpansion(!isExpanded)}
+            >
+                {isExpanded ? "less" : "...more"}
+            </button>
+        </p>
+    );
+  
+}
+
+export default ExpandableText
---
+++ src/components/message.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+
+interface Props {
+    children: ReactNode;
+    // color?: 'primary' | 'secondary' | 'danger';
+    // onClick: () => void;
+}
+
+function Message({children}:Props){
+    // const name = 'Mahid';
+    return <span>{children}</span>;
+}
+
+export default Message;
